769a23a9ab48b4ef87c3cd2e8c58c86711844a67
CharlesYoung <cnayoung@outlook.com>
2015-06-02 08:46:18 +0100

    Fixed unicode issues in comments across codebase

--- Resolution/Dictionaries/XmlSerializableDictionaryException.cs
+++ Resolution/Dictionaries/XmlSerializableDictionaryException.cs
@@ -1,9 +1,18 @@
 ﻿//----------------------------------------------------------------------------
 // <copyright file="XmlSerializableDictionaryException.cs" company="Solidsoft Reply Ltd">
-//   © 2015 Solidsoft Reply Ltd
-//   This software is released under the Apache 2 License.
-//   The license can be found in the file LICENSE at the root 
-//   directory of the distribution.
+//   Copyright 2015 Solidsoft Reply Limited.
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 // </copyright>
 //----------------------------------------------------------------------------
 
