e40a61283edc02e262a0dc51d9412907c9b5ee83
CharlesYoung <cnayoung@outlook.com>
2015-07-18 18:21:14 +0100

    Updated copyright notice

--- Resolution/ResolutionService/ActivityInterceptorConfiguration.cs
+++ Resolution/ResolutionService/ActivityInterceptorConfiguration.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ActivityInterceptorConfiguration.cs" company="Solidsoft Reply Ltd.">
-//   Copyright 2015 Solidsoft Reply Limited.
+//   Copyright (c) 2015 Solidsoft Reply Limited. All rights reserved.
 // 
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
